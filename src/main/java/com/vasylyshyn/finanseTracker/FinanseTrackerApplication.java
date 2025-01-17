079cc4e132dad79ba9fa4bdb070d0f507747cc21
VasylyshynDmytro <147658570+VasylyshynDmytro@users.noreply.github.com>
2025-01-17 17:43:17 +0200

    initinal commit

---
+++ src/main/java/com/vasylyshyn/finanseTracker/FinanseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vasylyshyn.finanseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasylyshyn/finanseTracker/FinanseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasylyshyn.finanseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
